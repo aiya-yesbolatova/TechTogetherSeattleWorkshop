9c57f1876989e9c910c0c246a7308519d0118b66
Akshaya-bit <69105579+Akshaya-bit@users.noreply.github.com>
2021-01-24 19:50:43 +0100

    Add files via upload

---
+++ Vision-master/app/src/main/java/com/example/choose/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.choose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+}
+}
